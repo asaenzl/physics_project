8ab780c249d2ec60695c043b2e5bda0822e41a33
asaenzl <asaenzl@ucenfotec.ac.cr>
2022-11-09 19:02:40 -0600

    Initial commit

---
+++ src/main.py
@@ -0,0 +1,79 @@
+import os
+import sys
+import math
+import time
+import pygame
+current_path = os.getcwd()
+import pymunk as pm
+from personajes import Objeto
+
+pygame.init()
+screen = pygame.display.set_mode((1200, 650))
+redbird = pygame.image.load(
+    "../resources/images/red-bird3.png").convert_alpha()
+background2 = pygame.image.load(
+    "../resources/images/background3.png").convert_alpha()
+sling_image = pygame.image.load(
+    "../resources/images/sling-3.png").convert_alpha()
+full_sprite = pygame.image.load(
+    "../resources/images/full-sprite.png").convert_alpha()
+rect = pygame.Rect(181, 1050, 50, 50)
+cropped = full_sprite.subsurface(rect).copy()
+pig_image = pygame.transform.scale(cropped, (30, 30))
+buttons = pygame.image.load(
+    "../resources/images/selected-buttons.png").convert_alpha()
+pig_happy = pygame.image.load(
+    "../resources/images/pig_failed.png").convert_alpha()
+stars = pygame.image.load(
+    "../resources/images/stars-edited.png").convert_alpha()
+
+
+rect = pygame.Rect(164, 10, 60, 60)
+pause_button = buttons.subsurface(rect).copy()
+rect = pygame.Rect(24, 4, 100, 100)
+replay_button = buttons.subsurface(rect).copy()
+rect = pygame.Rect(142, 365, 130, 100)
+next_button = buttons.subsurface(rect).copy()
+clock = pygame.time.Clock()
+rect = pygame.Rect(18, 212, 100, 100)
+play_button = buttons.subsurface(rect).copy()
+clock = pygame.time.Clock()
+running = True
+
+# the base of the physics
+space = pm.Space()
+space.gravity = (0.0, -700.0)
+pigs = []
+birds = []
+balls = []
+polys = []
+beams = []
+columns = []
+poly_points = []
+ball_number = 0
+polys_dict = {}
+mouse_distance = 0
+rope_lenght = 90
+angle = 0
+x_mouse = 0
+y_mouse = 0
+count = 0
+mouse_pressed = False
+t1 = 0
+tick_to_next_circle = 10
+RED = (255, 0, 0)
+BLUE = (0, 0, 255)
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+sling_x, sling_y = 135, 450
+sling2_x, sling2_y = 160, 450
+score = 0
+game_state = 0
+bird_path = []
+counter = 0
+restart_counter = False
+bonus_score_once = True
+bold_font = pygame.font.SysFont("arial", 30, bold=True)
+bold_font2 = pygame.font.SysFont("arial", 40, bold=True)
+bold_font3 = pygame.font.SysFont("arial", 50, bold=True)
+wall = False
